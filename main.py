9688e8f7d2a92a171ef066e12258cf14f7ec54e5
Yusuke Shibuya <shibuya@fun.bio.keio.ac.jp>
2022-01-04 23:48:11 +0900

    [update] camera app description

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import time
 
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(1)
 
 cascade_path = 'haarcascade_frontalface_alt2.xml'
 cascade = cv2.CascadeClassifier(cascade_path)
@@ -16,10 +16,10 @@ while True:
     if len(facerect) > 0:
         #検出した顔を囲む矩形の作成
         for rect in facerect:
-            cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
+            cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=4)
     cv2.imshow('myface',image)
 
     cv2.waitKey(1) 
-    time.sleep(0.2)
+    time.sleep(0.05)
     cv2.destroyAllWindows()
 
